664c75f1866cad60d1eb2f0d8acb6388fecfa118
anuj-weebjs <weeb.js@yahoo.com>
2024-07-31 00:03:44 +0530

    Added simple routes

---
+++ main.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const indexRoutes = require('./routes/index');
+const loginRoutes = require('./routes/login');
+const registerRoutes = require('./routes/register');
+
+app.set('view engine', 'ejs');
+
+
+app.use(express.static('public'))
+
+
+app.use('/', indexRoutes);
+app.use('/login', loginRoutes);
+app.use('/register', registerRoutes);
+
+
+
+const port = process.env.TOEKN || 4000;
+app.listen(port, () => {
+    console.log(`Server Started on http://localhost:4000/`);
+});
+
