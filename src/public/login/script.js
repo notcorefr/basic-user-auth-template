f9168acc024cfd182ba2c6c2ff5162ec348a690a
anuj-weebjs <weeb.js@yahoo.com>
2024-09-08 14:44:46 +0530

    Created HomePage

--- src/public/login/script.js
+++ src/public/login/script.js
@@ -23,7 +23,7 @@ if (submitBtn) {
         if (res.notify == 0) {
             setTimeout(() => {
                 window.location.reload();
-            }, 5000);
+            }, 3000);
         }
 
         // document.cookie = `sessionId=${res.metaData.session.id}; expires=${(res.metaData.session.expiresOn)} UTC; path=/`
